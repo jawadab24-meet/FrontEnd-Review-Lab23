fae68dd6d9af2453597c353ec6e7e94a787198e1
jawadab24-meet <jawadabusalih521@gmail.com>
2023-07-17 18:34:55 +0300

    fun

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function changeBackgroundColor(color) {
-	document.body.style.backgroundColor = color;
+function changeBackgroundColor(color) 
+{
+	if(document.getElementsByTagName('body')[0].style.backgroundColor == 'pink'){
+	document.getElementsByTagName('body')[0].style.backgroundColor = 'green';
 }
+
+	else { if(document.getElementsByTagName('body')[0].style.backgroundColor == 'green'){
+		document.getElementsByTagName('body')[0].style.backgroundColor='pink';
+		}
+	}
+}
+
+document.getElementsByTagName('body')[0].style.backgroundColor = 'pink'
